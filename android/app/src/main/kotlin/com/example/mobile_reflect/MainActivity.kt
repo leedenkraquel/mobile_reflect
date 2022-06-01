3f244147cfd320bc0c4beb701badd0d20d089a24
Leeden Raquel <77713467+leedenkraquel@users.noreply.github.com>
2022-06-01 10:48:23 -1000

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/mobile_reflect/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobile_reflect
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+/*
+ * Name: loginPage
+ * Author(s): Leeden Raquel
+ * Inputs:
+ *  None
+ * Description: this class represents the login page widget
+ * Returns:
+ *  None
+ */
+class loginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+
+}
